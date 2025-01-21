22ae2b30a827f7b1d0044c1f5161a4b4508c4b31
Rehman Ali <chrehmanali5@gmail.com>
2025-01-21 07:51:34 +0500

    169.Majority Elements

---
+++ Array Problems/Searching problems/majority_element.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    // https://leetcode.com/problems/majority-element
+       
+     vector<int> nums = {8,8,7,7,7};
+
+     int s = nums.size()/2;
+     int actualsize = nums.size();
+     int  i = 0;
+     int count=0;
+    for(i; i<actualsize; i++){
+     for(int j=0; j<actualsize;j++){
+          if(nums[i]== nums[j]){
+               count++;
+          }
+     }
+     if (count>s){
+          cout<<nums[i];
+          return nums[i];
+     }
+     count = 0;
+     
+     
+    }
+    return 0;
+     cout<<s;
+}

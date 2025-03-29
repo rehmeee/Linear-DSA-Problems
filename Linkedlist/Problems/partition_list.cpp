700aa360a586fe5b12a82456747657787fcd6d48
rehmeee <chrehmanali5@gmail.com>
2025-03-29 19:14:12 +0500

    partition list

---
+++ Linkedlist/Problems/partition_list.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<set>
+using namespace std;
+    class ListNode{
+        public:
+            int val;
+            ListNode* next;
+            ListNode(int val){
+                this->val = val;
+                next = NULL;
+            }
+    };
+    void printLL(ListNode* head){
+        ListNode* temp = head;
+        while (temp != nullptr)
+        {
+            cout<<temp->val<<endl;
+            temp = temp->next;
+        }
+    }
+ 
+ 
+    ListNode* partition(ListNode* head, int x) {
+        
+        ListNode* small = new ListNode(-1);
+        ListNode* large = new ListNode(-1);
+        ListNode* small_head = small;
+        ListNode* large_head = large;
+        
+        while (head){
+            if (head->val < x){
+                small->next = head;
+                small = small -> next;
+                head = head -> next;
+                small->next = NULL;
+            }
+            else{
+                large->next = head;
+                large = large -> next;
+                head = head -> next;
+                large -> next = NULL;
+            }
+        }
+        small -> next = large_head -> next;
+
+        return small_head -> next;
+    }
+int main(){
+        	ListNode* one = new ListNode(2);
+            ListNode* two = new ListNode(2);
+            ListNode* three = new ListNode(2);
+            ListNode* fourth = new ListNode(4);
+            ListNode* fifth = new ListNode(1);
+            ListNode* head = one;
+             one->next = two;
+            two->next = three;
+              three->next = fourth;
+            fourth->next = fifth;
+            head = partition(head, 4);
+            printLL(head);
+            
+}

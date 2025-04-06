eeea48d45e0cb4ba9b3bc3b511f3bdadeee329cf
rehmeee <chrehmanali5@gmail.com>
2025-04-06 11:10:39 +0500

    876. Middle of the Linked List

---
+++ Linkedlist/Problems/middle_of_linkedlist.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<set>
+using namespace std;
+    class ListNode{
+        public:
+            int val;
+            ListNode* next;
+            ListNode(int val){
+                this->val = val;
+                next = NULL;
+            }
+    };
+    void printLL(ListNode* head){
+        ListNode* temp = head;
+        while (temp != nullptr)
+        {
+            cout<<temp->val<<endl;
+            temp = temp->next;
+        }
+    }
+    ListNode* middleNode(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!= nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+int main(){
+        ListNode* one = new ListNode(10);
+        ListNode* two = new ListNode(20);
+        ListNode* three = new ListNode(30);
+        ListNode* fourth = new ListNode(40);
+        ListNode* fifth = new ListNode(50);
+        ListNode* head = one;
+         one->next = two;
+        two->next = three;
+          three->next = fourth;
+        fourth->next = fifth;	
+        head = middleNode(head);
+        
+}

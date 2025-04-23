8fd0cad7088bb9ad8e4f2890e40f842a011eb1e9
rehmeee <chrehmanali5@gmail.com>
2025-04-23 13:37:56 +0500

    179. Largest Number

---
+++ String Problems/largest_number.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<set>
+using namespace std;
+string largestNumber(vector<int>& nums) {
+         // Convert integers to strings
+         vector<string> array;
+         for (int num : nums) {
+             array.push_back(to_string(num));
+         }
+ 
+         // Custom comparator for sorting
+         sort(array.begin(), array.end(), [](const string &a, const string &b) {
+             return (b + a) < (a + b);
+         });
+ 
+         // Handle the case where the largest number is "0"
+         if (array[0] == "0") {
+             return "0";
+         }
+ 
+         // Build the largest number from the sorted array
+         string largest;
+         for (const string &num : array) {
+             largest += num;
+         }
+ 
+         return largest; 
+}
+int main(){
+        	vector<int> vec = {3,30,34,5,9};
+            cout<<largestNumber(vec)<<endl;
+}

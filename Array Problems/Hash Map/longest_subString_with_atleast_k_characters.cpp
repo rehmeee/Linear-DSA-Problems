46bfdebbd4c38e7a8c8b101e12e0a43653e0b730
rehmeee <chrehmanali5@gmail.com>
2025-08-21 00:17:03 +0500

    never solved this one

---
+++ Array Problems/Hash Map/longest_subString_with_atleast_k_characters.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<set>
+using namespace std;
+         int longestSubstring(string s, int k) {
+        
+    }	
+int main(){
+        string  s = "ababbc";
+        int k = 2	;
+        cout<<longestSubstring(s,k)<<endl;
+}
